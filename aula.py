6f8a464a3d0b187cc96d5374de31d3b63f5889c1
Maria Eduarda Martinelli <95435982+DudamaMartinelli@users.noreply.github.com>
2024-03-06 20:01:18 -0300

    Ajustes

--- aula.py
+++ aula.py
@@ -4,7 +4,7 @@ import random
 
 pygame.init()
 
-#Configuração da tela
+# Configuração da tela
 largura = 800
 altura = 600
 
@@ -19,15 +19,12 @@ VERDE = (0, 255, 0)
 AMARELO = (255, 255, 0)
 
 tamanho_fonte = 50
-fonte =pygame.font.SysFont(None, tamanho_fonte)
+fonte = pygame.font.SysFont(None, tamanho_fonte)
 
 texto = fonte.render("Maria", True, BRANCO)
 
 texto_rect = texto.get_rect(center=(largura / 2, altura / 2))
 clock = pygame.time.Clock()
-
-#velocidade_x = 1
-#velocidade_y = 1
 
 velocidade_x = random.randint(-1, 1)
 velocidade_y = random.randint(-1, 1)
@@ -37,39 +34,25 @@ while velocidade_x == 0:
 while velocidade_y == 0:
     velocidade_y = random.randint(-1, 1)
 
-
-#Loop principal
+# Loop principal
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-
     texto_rect.x += velocidade_x
-
-    if texto_rect.right >= largura:
-        velocidade_x = random.randint(-1, 0)
-        velocidade_y = random.randint(-1, 1)
-        texto = fonte.render("Maria", True, VERMELHO)
-
-    if texto_rect.left <= 1:
-        velocidade_x = random.randint(0, 1)
-        velocidade_y = random.randint(-1, 1)
-        texto = fonte.render("Maria", True, VERDE)
-    
     texto_rect.y += velocidade_y
 
-    if texto_rect.bottom <= 1:
-        velocidade_x = random.randint(-1, 1)
-        velocidade_y = random.randint(-1, 1)
-        texto = fonte.render("Maria", True, AMARELO)
+    # Verifica as bordas da tela para a direita e esquerda
+    if texto_rect.right >= largura or texto_rect.left <= 0:
+        velocidade_x = -velocidade_x  # Inverte a direção horizontal
+        texto = fonte.render("Maria", True, VERMELHO if velocidade_x > 0 else VERDE)
 
-    if texto_rect.top >= altura:
-        velocidade_x = random.randint(-1, 1)
-        velocidade_y = random.randint(-1, 1)
-        texto = fonte.render("Maria", True, AZUL)
-
+    # Verifica as bordas da tela para cima e para baixo
+    if texto_rect.bottom >= altura or texto_rect.top <= 0:
+        velocidade_y = -velocidade_y  # Inverte a direção vertical
+        texto = fonte.render("Maria", True, AMARELO if velocidade_y > 0 else AZUL)
 
     clock.tick(120)
     tela.fill(PRETO)
